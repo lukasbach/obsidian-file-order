a0ff6a8b101a1a357eb3f8f5dea30efa513dc0a9
Brian J. Cardiff <bcardiff@gmail.com>
2023-09-21 22:32:11 -0300

    Add ignore pattern setting to ignore files and folder to reorder

--- src/common.ts
+++ src/common.ts
@@ -2,9 +2,11 @@ export interface FileOrderSettings {
   delimiter: string;
   prefixMinLength: number;
   startingIndex: number;
+  ignorePattern: string;
 }
 export const DEFAULT_SETTINGS: FileOrderSettings = {
   delimiter: " ",
   prefixMinLength: 0,
   startingIndex: 0,
+  ignorePattern: "",
 };
